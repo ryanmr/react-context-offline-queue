b39ca8614b0a832c7889d6eda698316262557639
Ryan Rampersad <ryan.rampersad@gmail.com>
2021-01-18 23:46:25 -0600

    Adds code

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import delay from 'delay';
+import React, { useState, useEffect, ReactChildren, useRef } from 'react';
 import './App.css';
+import { OfflineProvider, useOffline } from './offline';
+import { useOnlineStatus } from './offline-hooks';
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+function Providers({ children }: ProvidersProps) {
+  return <>{children}</>;
+}
 
 interface AppProps {}
-
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+  const [x, setX] = useState(-10);
+  const [y, setY] = useState(10);
+
+  async function doX(data: any) {
+    await delay(250 + Math.random() * 1000);
+    setX((p) => p + data.value);
+  }
+
+  async function doY(data: any) {
+    await delay(250 + Math.random() * 1000);
+    setY((p) => p + data.value);
+  }
+
+  const registeration = {
+    addx: { key: 'addx', fn: doX },
+    addy: { key: 'addy', fn: doY },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <OfflineProvider registeration={registeration}>
+      <div className="App">
+        <UserInterface x={x} y={y} />
+      </div>
+    </OfflineProvider>
+  );
+}
+
+interface UserInterfaceProps {
+  x: number;
+  y: number;
+}
+function UserInterface({ x, y }: UserInterfaceProps) {
+  const { queue } = useOffline();
+  const online = useOnlineStatus();
+  return (
+    <>
+      <button
+        onClick={() => {
+          queue('addx', { value: 1 });
+        }}
+      >
+        add x
+      </button>
+      <button
+        onClick={() => {
+          queue('addy', { value: 1 });
+        }}
+      >
+        add y
+      </button>
+      <hr />
+      <p>x: {x}</p>
+      <p>y: {y}</p>
+      <hr />
+      <p>online: {online ? 'true' : 'false'}</p>
+    </>
   );
 }
 
---
+++ src/offline.tsx
@@ -0,0 +1,158 @@
+import React, {
+  createContext,
+  ReactChildren,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { useOnlineStatus } from './offline-hooks';
+
+interface OfflineContextValue {
+  // register(key: string, fn: Function): void;
+  // unregister(key: string): void;
+  queue(key: string, data: any): void;
+}
+
+const OfflineContext = createContext<OfflineContextValue | null>(null);
+
+interface MappingElement {
+  key: string;
+  fn: Function;
+}
+
+interface Mapping {
+  [key: string]: MappingElement;
+}
+
+enum QueueStatus {
+  pending = 'pending',
+  done = 'done',
+}
+
+interface QueueElement {
+  key: string;
+  data: any;
+  status: QueueStatus;
+}
+
+type QueueList = QueueElement[];
+
+function persist(queueList: QueueList) {
+  window.localStorage.setItem('_offline_actions', JSON.stringify(queueList));
+}
+
+function inflate() {
+  const content = window.localStorage.getItem('_offline_actions');
+  if (content) {
+    const data = JSON.parse(content);
+    return data;
+  }
+  return null;
+}
+
+interface Props {
+  registeration: Mapping;
+  children: React.ReactNode;
+}
+export function OfflineProvider({ registeration, children }: Props) {
+  const online = useOnlineStatus();
+  // const mapRef = useRef<Mapping>({});
+
+  const [counter, setCounter] = useState(0);
+  const [inflated, setInflated] = useState(false);
+  const [queueList, setQueueList] = useState<QueueList>([]);
+
+  // function register(key: string, fn: Function) {
+  //   const el: MappingElement = {
+  //     key,
+  //     fn,
+  //   };
+  //   mapRef.current[key] = el;
+  // }
+
+  // function unregister(key: string) {
+  //   delete mapRef.current[key];
+  // }
+
+  function queue(key: string, data: any) {
+    if (!registeration[key]) {
+      throw new Error(`no mapping for ${key}`);
+    }
+
+    const el: QueueElement = {
+      key,
+      data,
+      status: QueueStatus.pending,
+    };
+
+    setCounter((p) => p + 1);
+    setQueueList((p) => [...p, el]);
+  }
+
+  async function process() {
+    for (const element of queueList) {
+      if (
+        registeration[element.key] &&
+        element.status === QueueStatus.pending
+      ) {
+        try {
+          await registeration[element.key].fn(element.data);
+
+          // is this OK
+          // mutate a reference inside of an array tracked by
+          // useState seems very bad
+          element.status = QueueStatus.done;
+
+          persist(queueList);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+
+    // filters the list so it does not grow too big with all of these done entries
+    const pendingQueueList = queueList.filter(
+      (el) => el.status === QueueStatus.pending,
+    );
+    setQueueList(pendingQueueList);
+  }
+
+  useEffect(() => {
+    if (!online) {
+      console.info('offline provider: not online');
+      return;
+    }
+
+    if (!inflated) {
+      console.info('offline provider: queue not inflated yet');
+      return;
+    }
+
+    persist(queueList);
+    process().then(() => {
+      console.info('offline provider: processing done');
+    });
+  }, [counter, online]);
+
+  useEffect(() => {
+    const stored = inflate();
+    if (stored) {
+      setInflated(true);
+      setCounter((p) => p + 1);
+      setQueueList(stored);
+      console.info('offline provider: queue inflated');
+    }
+  }, []);
+
+  const value = { queue };
+
+  return (
+    <OfflineContext.Provider value={value}>{children}</OfflineContext.Provider>
+  );
+}
+
+export function useOffline() {
+  const context = useContext(OfflineContext) as OfflineContextValue;
+  return context;
+}
